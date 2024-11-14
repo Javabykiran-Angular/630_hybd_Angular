68e10558d6b1d270000f80ad01356b1931ee2c30
The Kiran Academy <71135924+Javabykiran-Angular@users.noreply.github.com>
2024-11-14 13:34:28 +0530

    Day06

---
+++ Day06/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { StringinterpolationComponent } from './stringinterpolation/stringinterpolation.component';
+import { PropertybindingComponent } from './propertybinding/propertybinding.component';
+import { StylebindingComponent } from './stylebinding/stylebinding.component';
+import { ClassbindingComponent } from './classbinding/classbinding.component';
+import { EventbindingComponent } from './eventbinding/eventbinding.component';
+import { Eventbinding2Component } from './eventbinding2/eventbinding2.component';
+import { FormsModule } from '@angular/forms';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeeComponent,
+    StringinterpolationComponent,
+    PropertybindingComponent,
+    StylebindingComponent,
+    ClassbindingComponent,
+    EventbindingComponent,
+    Eventbinding2Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
