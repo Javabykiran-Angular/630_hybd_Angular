12388a427c8f144664f7e3f0c054485a25691042
The Kiran Academy <71135924+Javabykiran-Angular@users.noreply.github.com>
2024-12-04 11:51:16 +0530

    Update

---
+++ Day17/tdf/tdf.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-tdf',
+  templateUrl: './tdf.component.html',
+  styleUrls: ['./tdf.component.css']
+})
+export class TdfComponent {
+
+
+
+  EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+  + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+  onSubmit(f:NgForm){
+    console.log(f.value.myusername)
+  }
+
+}
