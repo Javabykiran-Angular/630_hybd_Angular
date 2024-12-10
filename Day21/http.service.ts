cba105a6308cd9397cf977f96f90e426cf22ed0d
The Kiran Academy <71135924+Javabykiran-Angular@users.noreply.github.com>
2024-12-10 12:30:33 +0530

    Day21

---
+++ Day21/http.service.ts
@@ -0,0 +1,42 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  baseUrl:string='http://localhost:8080/api/';
+  constructor(private http:HttpClient) { }
+
+  getAllEmp(){
+   return (this.http.get(`${this.baseUrl}getAllEmp`));
+  }
+
+  getEmpById(id:any){
+    return (this.http.get(`${this.baseUrl}getparticularEmpById/${id}`));
+  }
+
+  getAllCountry(){
+    return (this.http.get(`${this.baseUrl}getAllCountry`));
+  }
+
+  postEmpData(obj:any){
+     return (this.http.post(`${this.baseUrl}addemp`,obj,{
+        responseType:'text'
+      }));
+  }
+
+  updateEmpData(obj:any){
+     return (this.http.put(`${this.baseUrl}updateEmp/${obj.id}`,obj,{
+        responseType:'text'
+      }));
+  }
+
+  deleteData(id:any){
+   return (this.http.delete(`${this.baseUrl}deleteemp/${id}`,{
+      responseType:'text'
+    }));
+  }
+
+}
